3ea2f4e5e74b946037a7f07200a1f91e4fcbca46
Allison Durham <github-actions[bot]@users.noreply.github.com>
2024-08-30 09:39:47 -0700

    new examples, new build.zig

--- build.zig
+++ build.zig
@@ -7,21 +7,6 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    // Define the generate_providers step
-    // const generate_providers = b.addExecutable(.{
-    //     .name = "generate_providers",
-    //     .root_source_file = b.path("tools/generate_providers.zig"),
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-
-    // Create a run step for fetch_models
-    // const run_generate_providers = b.addRunArtifact(generate_providers);
-
-    // Create a generate step that depends on running fetch_models
-    // const generate_step = b.step("generate", "Generate provider models");
-    // generate_step.dependOn(&run_generate_providers.step);
-
     // Create the zai module
     const zai_mod = b.addModule(package_name, .{
         .root_source_file = b.path(package_path),
@@ -29,10 +14,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    // Add the generate step as a dependency of any step that uses the zai module
-    // b.getInstallStep().dependOn(generate_step);
-
-    //example section
+    // Example section
     const chat = b.addExecutable(.{
         .name = "chat",
         .root_source_file = b.path("examples/chat.zig"),
@@ -42,20 +24,20 @@ pub fn build(b: *std.Build) void {
 
     chat.root_module.addImport("zai", zai_mod);
     const build_chat = b.addInstallArtifact(chat, .{});
-    const build_chat_step = b.step("chat", "build the chat completion example.");
+    const build_chat_step = b.step("chat", "build the chat example.");
     build_chat_step.dependOn(&build_chat.step);
 
-    const chat_completion = b.addExecutable(.{
-        .name = "chat_completion",
-        .root_source_file = b.path("examples/chat_completion.zig"),
+    const completion = b.addExecutable(.{
+        .name = "completion",
+        .root_source_file = b.path("examples/completion.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    chat_completion.root_module.addImport("zai", zai_mod);
-    const build_chat_completion = b.addInstallArtifact(chat_completion, .{});
-    const build_chat_completion_step = b.step("chat-completion", "build the chat completion example.");
-    build_chat_completion_step.dependOn(&build_chat_completion.step);
+    completion.root_module.addImport("zai", zai_mod);
+    const build_completion = b.addInstallArtifact(completion, .{});
+    const build_completion_step = b.step("completion", "build the completion example.");
+    build_completion_step.dependOn(&build_completion.step);
 
     const embeddings = b.addExecutable(.{
         .name = "embeddings",
--- examples/chat.zig
+++ examples/chat.zig
@@ -39,30 +39,6 @@ pub fn main() !void {
     defer allocator.free(chat_response);
     std.debug.print("Chat response: {s}\n", .{chat_response});
 
-    const completion_options = zai.CompletionRequestOptions{
-        .model = "meta-llama-3.1-8b-instruct",
-        .prompt = "Hello, I'm exploring the wonderful land of",
-        .temperature = 0.7,
-        .top_p = 1.0,
-        .stream = false,
-    };
-
-    const completion_response = try provider.completion(completion_options);
-    defer allocator.free(completion_response);
-    std.debug.print("Completion response: {s}\n", .{completion_response});
-
-    const streaming_completion_options = zai.CompletionRequestOptions{
-        .model = "meta-llama-3.1-8b-instruct",
-        .prompt = "Following this line is a long story about zig, the programming language:\n",
-        .temperature = 0.7,
-        .max_tokens = 1500,
-        .top_p = 1.0,
-        .stream = true,
-    };
-    const stdout = std.io.getStdOut().writer();
-
-    try provider.completionStream(streaming_completion_options, stdout);
-
     const streaming_chat_options = zai.ChatRequestOptions{
         .model = "meta-llama-3.1-8b-instruct",
         .messages = &[_]zai.Message{
@@ -82,5 +58,5 @@ pub fn main() !void {
     };
 
     std.debug.print("\n\nStreaming chat response:\n", .{});
-    try provider.chatStream(streaming_chat_options, stdout);
+    try provider.chatStream(streaming_chat_options, std.io.getStdOut().writer());
 }
